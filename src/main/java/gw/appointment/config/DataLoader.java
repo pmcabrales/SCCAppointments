72a684f29443f6970db03151581bc36340f147ec
Pedro Martínez Cabrales <>
2025-01-21 10:47:53 +0100

    serviceSkills dataload

--- src/main/java/gw/appointment/config/DataLoader.java
+++ src/main/java/gw/appointment/config/DataLoader.java
@@ -1,23 +1,31 @@
 package gw.appointment.config;
 
 import gw.appointment.entity.Appointment;
+import gw.appointment.entity.Service;
+import gw.appointment.entity.ServiceSkill;
 import gw.appointment.entity.Skill;
 import gw.appointment.repository.AppointmentRepository;
 import gw.appointment.repository.ServiceRepository;
 import gw.appointment.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.HashSet;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
-    private final AppointmentRepository appointmentRepository;
-    private final ServiceRepository serviceRepository;
-    private final SkillRepository skillRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+    @Autowired
+    private ServiceRepository serviceRepository;
+    @Autowired
+    private SkillRepository skillRepository;
+
 
     public DataLoader(AppointmentRepository appointmentRepository, ServiceRepository serviceRepository, SkillRepository skillRepository){
         this.appointmentRepository = appointmentRepository;
@@ -27,20 +35,42 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         loadAppointments();
-        loadEssentialServices();
         loadSkills();
+        loadServices();
     }
 
     private void loadAppointments() {
         appointmentRepository.save(new Appointment(1,LocalDateTime.of(LocalDate.of(2025,1,16), LocalTime.of(12,30)),"Test Appointment"));
     }
 
-    private void loadEssentialServices(){
-       /* essentialServiceRepository.save(new EssentialService(1, "GWCP Observability"));
-        essentialServiceRepository.save(new EssentialService(2, "Performance Diagnostics"));
-        essentialServiceRepository.save(new EssentialService(3, "Platform Health Review"));
-        essentialServiceRepository.save(new EssentialService(4, "Production Transition Enablement"));*/
+    private void loadServices(){
+        Service service = serviceRepository.save(new Service(1L, "GWCP Observability", new HashSet<>()));
+        service.getSkills().add(new ServiceSkill(1L,service, skillRepository.getReferenceById(1L), 40));
+        serviceRepository.save(service);
+
+        service = serviceRepository.save(new Service(2L, "Performance Diagnostics", new HashSet<>()));
+        service.getSkills().add(new ServiceSkill(2L,service, skillRepository.getReferenceById(1L), 16));
+        serviceRepository.save(service);
+        service.getSkills().add(new ServiceSkill(3L,service, skillRepository.getReferenceById(2L), 80));
+        serviceRepository.save(service);
+        service.getSkills().add(new ServiceSkill(4L,service, skillRepository.getReferenceById(3L), 20));
+        serviceRepository.save(service);
+
+        service = serviceRepository.save(new Service(3L, "Platform Health Review", new HashSet<>()));
+        service.getSkills().add(new ServiceSkill(5L,service, skillRepository.getReferenceById(4L), 100));
+        serviceRepository.save(service);
+        service.getSkills().add(new ServiceSkill(6L,service, skillRepository.getReferenceById(5L), 20));
+        serviceRepository.save(service);
+
+        service = serviceRepository.save(new Service(4L, "Production Transition Enablement", new HashSet<>()));
+        service.getSkills().add(new ServiceSkill(7L,service, skillRepository.getReferenceById(6L), 40));
+        serviceRepository.save(service);
+        service.getSkills().add(new ServiceSkill(8L,service, skillRepository.getReferenceById(7L), 8));
+        serviceRepository.save(service);
+        service.getSkills().add(new ServiceSkill(9L,service, skillRepository.getReferenceById(8L), 8));
+        serviceRepository.save(service);
     }
+
 
     private void loadSkills(){
         skillRepository.save(new Skill(1L, "ES-Observability-Dev_3"));
