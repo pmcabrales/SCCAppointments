e123c2d80f3b7e54b0f1153e044ef9495598d0f4
Pedro Martínez Cabrales <>
2025-01-21 15:09:50 +0100

    refactoring

--- src/main/java/gw/appointment/controller/ResourceController.java
+++ src/main/java/gw/appointment/controller/ResourceController.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/resources")
 public class ResourceController {
 
     @Autowired
@@ -23,31 +25,31 @@ public class ResourceController {
         this.resourceRepository = resourceRepository;
     }
 
-    @GetMapping("/resourceList")
+    @GetMapping()
     public String showResourceList(Model model) {
         List<Resource> resources = resourceRepository.findAll();
         model.addAttribute("resources", resources);
-        model.addAttribute("contentFragment", "resources/resource_list");
+        model.addAttribute("contentFragment", "resources/resources");
         return "layout";
     }
 
-    @GetMapping("/registerResource")
+    @GetMapping("/new")
     public String showForm(Model model) {
         List<Skill> skills = skillRepository.findAll();
         model.addAttribute("resource", new Resource());
         model.addAttribute("skillsList", skills); // Pass skills from DB to the view
 
-        model.addAttribute("contentFragment", "resources/resource_register");
+        model.addAttribute("contentFragment", "resources/resource-form");
         return "layout";
     }
 
-    @PostMapping("/registerResource")
+    @PostMapping("/new")
     public String submitForm(Resource resource, Model model) {
         resourceRepository.save(resource);
         model.addAttribute("message", "Resource registered successfully!");
         List<Resource> resources = resourceRepository.findAll();
         model.addAttribute("resources", resources);
-        model.addAttribute("contentFragment", "resources/resource_list");
+        model.addAttribute("contentFragment", "resources/resources");
         return "layout";
     }
 }
